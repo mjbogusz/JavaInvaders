36dea82f67acd795e3bce03c64ce95a56e2dbf8b
mjbogusz <mjbogusz+github@gmail.com>
2015-12-14 23:08:23 +0100

    ModelDTO: EnemyDTO and ObstaclesDTO implemented.

--- pl/mbogusz3/invaders/model/InvadersModelDTO.java
+++ pl/mbogusz3/invaders/model/InvadersModelDTO.java
@@ -1,16 +1,18 @@
 package pl.mbogusz3.invaders.model;
 
-import pl.mbogusz3.invaders.model.InvadersModel;
-import pl.mbogusz3.invaders.model.Player;
 import java.io.Serializable;
 
 /**
  */
 public class InvadersModelDTO implements Serializable {
 	public final PlayerDTO player;
+	public final EnemyDTO enemy;
+	public final ObstaclesDTO obstacles;
 
 	public InvadersModelDTO(InvadersModel model) {
 		this.player = new PlayerDTO(model.getPlayer());
+		this.enemy = new EnemyDTO(model.getEnemy());
+		this.obstacles = new ObstaclesDTO(model.getObstacles());
 	}
 
 	public class PlayerDTO {
@@ -37,7 +39,75 @@ public class InvadersModelDTO implements Serializable {
 		}
 	}
 
+	public class EnemyDTO {
+		public final int rows;
+		public final int columns;
+		public final boolean[][] state;
+		public final double positionTop;
+		public final double positionLeft;
+
+		public EnemyDTO(Enemy enemy) {
+			this.rows = enemy.getRows();
+			this.columns = enemy.getColumns();
+			this.state = enemy.getState();
+			this.positionTop = enemy.getPositionTop();
+			this.positionLeft = enemy.getPositionLeft();
+		}
+
+		public int getRows() {
+			return rows;
+		}
+
+		public int getColumns() {
+			return columns;
+		}
+
+		public boolean[][] getState() {
+			return state;
+		}
+
+		public double getPositionTop() {
+			return positionTop;
+		}
+
+		public double getPositionLeft() {
+			return positionLeft;
+		}
+	}
+
+	public class ObstaclesDTO {
+		public final int count;
+		public final int strength;
+		public final int[] state;
+
+		public ObstaclesDTO(Obstacles obstacles) {
+			this.count = obstacles.getCount();
+			this.strength = obstacles.getStrength();
+			this.state = obstacles.getState();
+		}
+
+		public int getCount() {
+			return count;
+		}
+
+		public int getStrength() {
+			return strength;
+		}
+
+		public int[] getState() {
+			return state;
+		}
+	}
+
 	public PlayerDTO getPlayer() {
 		return player;
+	}
+
+	public EnemyDTO getEnemy() {
+		return enemy;
+	}
+
+	public ObstaclesDTO getObstacles() {
+		return obstacles;
 	}
 }
