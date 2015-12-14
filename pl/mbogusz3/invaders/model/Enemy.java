2768d080a5101b59dd839ab214abfeb155d49117
mjbogusz <mjbogusz+github@gmail.com>
2015-12-14 23:06:47 +0100

    Enemy: class implemented - stores whole enemy army.

--- pl/mbogusz3/invaders/model/Enemy.java
+++ pl/mbogusz3/invaders/model/Enemy.java
@@ -1,8 +1,48 @@
 package pl.mbogusz3.invaders.model;
 
+import java.util.Arrays;
+
 /**
  *
  */
 public class Enemy {
-	
+	private final int rows;
+	private final int columns;
+	private boolean[][] state;
+	private double positionTop;
+	private double positionLeft;
+
+	public Enemy(int rows, int columns) {
+		this.rows = rows;
+		this.columns = columns;
+		this.state = new boolean[rows][columns];
+	}
+
+	public void respawn() {
+		for(boolean[] s : state) {
+			Arrays.fill(s, true);
+		}
+		this.positionTop = 0.0;
+		this.positionLeft = 0.5;
+	}
+
+	public boolean[][] getState() {
+		return this.state;
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+
+	public double getPositionTop() {
+		return positionTop;
+	}
+
+	public double getPositionLeft() {
+		return positionLeft;
+	}
 }
