0fa879cc3ff06e4a22a923b078b91fcbed18d1fa
mjbogusz <mjbogusz+github@gmail.com>
2016-01-12 18:06:45 +0100

    Projectile: added width and height public statics (and passing them to DTO); added destroy() method

--- pl/mbogusz3/invaders/DTO/ProjectileDTO.java
+++ pl/mbogusz3/invaders/DTO/ProjectileDTO.java
@@ -8,11 +8,15 @@ public class ProjectileDTO {
 	public final int orientation;
 	public final double positionX;
 	public final double positionY;
+	public final double width;
+	public final double height;
 
 	public ProjectileDTO(Projectile projectile) {
 		this.orientation = projectile.getOrientation();
 		this.positionX = projectile.getPositionX();
 		this.positionY = projectile.getPositionY();
+		this.width = Projectile.width;
+		this.height = Projectile.height;
 	}
 
 	public int getOrientation() {
@@ -25,5 +29,13 @@ public class ProjectileDTO {
 
 	public double getPositionY() {
 		return positionY;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public double getHeight() {
+		return height;
 	}
 }
