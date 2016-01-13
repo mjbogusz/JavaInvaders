1ff39e0a67adf017bcbad93674dee2d46804a864
mjbogusz <mjbogusz+github@gmail.com>
2016-01-13 21:02:41 +0100

    Obstacles: implemented damage() method, added public static default health and count; added passing of height and positionTop through DTO

--- pl/mbogusz3/invaders/DTO/ObstaclesDTO.java
+++ pl/mbogusz3/invaders/DTO/ObstaclesDTO.java
@@ -5,13 +5,25 @@ import pl.mbogusz3.invaders.model.Obstacles;
 /**
  */
 public class ObstaclesDTO {
+	public final double positionTop;
+	public final double height;
 	public final int count;
 	public final int[] state;
 
 	public ObstaclesDTO(Obstacles obstacles) {
+		this.positionTop = Obstacles.positionTop;
+		this.height = Obstacles.height;
 		this.count = obstacles.getCount();
 		this.state = new int[this.count];
 		System.arraycopy(obstacles.getState(), 0, this.state, 0, this.count);
+	}
+
+	public double getPositionTop() {
+		return positionTop;
+	}
+
+	public double getHeight() {
+		return height;
 	}
 
 	public int getCount() {
--- pl/mbogusz3/invaders/model/Obstacles.java
+++ pl/mbogusz3/invaders/model/Obstacles.java
@@ -6,33 +6,49 @@ import java.util.Arrays;
  *
  */
 public class Obstacles {
-	public final static int defaultStrength = 4;
+	public final static int defaultCount = 3;
+	public final static int defaultHealth = 4;
 	public final static double height = 0.1;
 	public final static double positionTop = 0.85;
 	private final int count;
-	private final int strength;
-	private final int[] state;
+	private final int maxHealth;
+	private int[] state;
 
-	public Obstacles(int count) {
-		this(count, Obstacles.defaultStrength);
+	public Obstacles() {
+		this(Obstacles.defaultCount);
 	}
 
-	public Obstacles(int count, int strength) {
+	public Obstacles(int count) {
+		this(count, Obstacles.defaultHealth);
+	}
+
+	public Obstacles(int count, int maxHealth) {
 		this.count = count;
-		this.strength = strength;
+		this.maxHealth = maxHealth;
 		this.state = new int[count];
 	}
 
 	public void respawn() {
-		Arrays.fill(this.state, this.strength);
+		Arrays.fill(this.state, this.maxHealth);
+	}
+
+	public void damage(int index) {
+		this.damage(index, 1);
+	}
+
+	public void damage(int index, int damage) {
+		if(index < 0 || index > this.count) {
+			return;
+		}
+
+		this.state[index] -= damage;
+		if(this.state[index] < 0) {
+			this.state[index] = 0;
+		}
 	}
 
 	public int getCount() {
 		return count;
-	}
-
-	public int getStrength() {
-		return strength;
 	}
 
 	public int[] getState() {
